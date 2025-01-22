61aed9c773bcb8677ff407c6e0d0bc7f57f0fb3b
Hamid <gmhamid6@gmail.com>
2025-01-22 16:48:30 +0330

    Add project files.

---
+++ Noyan_Task.API/Entities/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Noyan_Task.API.Entities
+{
+    public class Blog
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public string Author { get; set; }
+    }
+}
